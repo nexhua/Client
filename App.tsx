37483130170d1c5333ddce37139136b8d371e8a6
nexhua <CChristofear@hotmail.com>
2023-04-04 05:09:05 +0300

    eslint and prettier configs, ts compiler settings

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {Button, Provider as PaperProvider} from 'react-native-paper';
 
 function App(): JSX.Element {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <PaperProvider>
+      <View>
+        <Text>Hello World</Text>
+        <Text>Hello World</Text>
+        <Text>Hello World</Text>
+        <Text>Hello World</Text>
+        <Text>Hello World</Text>
+        <Text>Hello World</Text>
+        <Button
+          icon="camera"
+          mode="contained-tonal"
+          onPress={() => {
+            console.log('pressed');
+          }}>
+          Press Me
+        </Button>
+      </View>
+    </PaperProvider>
   );
 }
 
